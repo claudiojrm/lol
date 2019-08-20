0506cb00dae05d83276e446786da002ef3fcaf45
Cláudio Jr. Melo <cjsilva@uolinc.com>
2019-08-20 20:17:13 -0300

    lance alance admin

--- aovivo/admin.py
+++ aovivo/admin.py
@@ -2,14 +2,18 @@ from django.contrib import admin
 from aovivo.models import Partida, Lance
 from adminsortable2.admin import SortableInlineAdminMixin
 
-class LanceAdmin(SortableInlineAdminMixin, admin.StackedInline):
+class LanceInlineAdmin(SortableInlineAdminMixin, admin.StackedInline):
     model = Lance
     extra = 1
 
+class LanceAdmin(admin.ModelAdmin):
+    list_display = ('partida', 'titulo', 'status')
+    ordering = ['-partida', '-status']
+
 class PartidaAdmin(admin.ModelAdmin):
     list_display = ('titulo', 'slugs')
-    inlines = (LanceAdmin, )
     autocomplete_fields = ('campeonato',)
+    inlines = (LanceInlineAdmin, )
     
     fields = (
         'titulo',
@@ -24,3 +28,4 @@ class PartidaAdmin(admin.ModelAdmin):
         return '/'.join(('', obj.campeonato.regiao.slug,obj.campeonato.slug, obj.slug, ''))
 
 admin.site.register(Partida, PartidaAdmin)
+admin.site.register(Lance, LanceAdmin)
